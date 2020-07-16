077dbeb673e940c0441f213c520bd0d29362700c
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2020-07-16 14:24:42 -0400

    Initial Commit

---
+++ Active_Directory/GetDomainInfo.c
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <stdio.h>
+#include <dsgetdc.h>
+#include "beacon.h"
+
+DECLSPEC_IMPORT DWORD WINAPI NETAPI32$DsGetDcNameA(LPVOID, LPVOID, LPVOID, LPVOID, ULONG, LPVOID);
+DECLSPEC_IMPORT DWORD WINAPI NETAPI32$NetApiBufferFree(LPVOID);
+
+void go(char * args, int alen) {
+	DWORD dwRet;
+	PDOMAIN_CONTROLLER_INFO pdcInfo;
+
+	dwRet = NETAPI32$DsGetDcNameA(NULL, NULL, NULL, NULL, 0, &pdcInfo);
+	if (ERROR_SUCCESS == dwRet) {
+        BeaconPrintf(CALLBACK_OUTPUT, "Domain Forest Name: %s\n"
+        "Domain: %s\n"
+        "Domain Controller: %s\n"
+        "Domain Controller Address: %s\n"
+        "Domain GUID: %s\n"
+        "DC Site Name: %s\n",
+        pdcInfo->DnsForestName,
+        pdcInfo->DomainName,
+        pdcInfo->DomainControllerName,
+        pdcInfo->DomainControllerAddress,
+        pdcInfo->DomainGuid,
+        pdcInfo->DcSiteName);        
+	}
+
+	NETAPI32$NetApiBufferFree(pdcInfo);
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BOF_Collection
  Various Cobalt Strike BOFs
+
+## Active_Directory
+
+### GetDomainInfo
+Returns information on the current domain and domain controller.
+
+### beacon.h
+You'll need [beacon.h](https://www.cobaltstrike.com/downloads/beacon.h)
